ec4540d559207a18b2603369ec041f06cb3e458d
Jean René RANDRIANJAKAMANANA <93261913+njaka337@users.noreply.github.com>
2024-11-28 20:50:51 +0300

    Update and rename portfolio1.js to portfolio.js

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+function submit(){
+  let mail=document.getElementById("mail");
+  let pass=document.getElementById("pass");
+  if(confirm("Would you show the Informations ?")){
+    alert(mail);
+    alert(pass);
+  }
+}
--- portfolio1.js
+++
@@ -1,6 +0,0 @@
-if(comfirm("Would you come in ?")){
-  alert("Hello! Tongasoa");
-}
-else{
-  alert("Thanks for visiting");
-}
