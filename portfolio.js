4da091be682290c084ef7c7a0829edf4168784f6
Jean René RANDRIANJAKAMANANA <93261913+njaka337@users.noreply.github.com>
2024-12-04 19:24:23 +0300

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,11 +1,11 @@
 let sub=document.getElementById("submit");
 sub.addEventListener("click", submit, false);
-let mail=document.getElementById("mail");
-let pass=document.getElementById("pass");
 
 function submit(){
+  let mail=document.getElementById("mail");
+  let pass=document.getElementById("pass");
   if(confirm("Would you show the Informations ?")){
-    alert(mail.getAttribute("value");
-    alert(pass.getAttribute("value");
+    alert(mail.getAtttribute("value"));
+    alert(pass.getAttribute("value"));
   }
 }
