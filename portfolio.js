822b8b215a9f07acb969a6a748f9c99d754f9612
Jean René RANDRIANJAKAMANANA <93261913+njaka337@users.noreply.github.com>
2024-12-04 19:17:18 +0300

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -5,7 +5,7 @@ function submit(){
   let mail=document.getElementById("mail");
   let pass=document.getElementById("pass");
   if(confirm("Would you show the Informations ?")){
-    alert(mail);
-    alert(pass);
+    alert(mail.value);
+    alert(pass.value);
   }
 }
