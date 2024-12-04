f2d7793c78395069d8874cf88b1e75ce6f4c7354
Jean René RANDRIANJAKAMANANA <93261913+njaka337@users.noreply.github.com>
2024-12-04 19:27:15 +0300

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,9 +1,9 @@
 let sub=document.getElementById("submit");
 sub.addEventListener("click", submit, false);
+let mail=p.getElementById("mail");
+let pass=document.getElementById("pass");
 
 function submit(){
-  let mail=document.getElementById("mail");
-  let pass=document.getElementById("pass");
   if(confirm("Would you show the Informations ?")){
     alert(mail.getAtttribute("value"));
     alert(pass.getAttribute("value"));
